f8c69c76ed95892da500fabb4675c7f02c86f726
Martyna Markiel <mail@gft.com>
2021-05-04 22:18:04 +0200

    initialize sorting app

---
+++ sort/build/index.js
@@ -0,0 +1,24 @@
+"use strict";
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        // const length = this.collection.length - DESTRUCTURED:
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    // swaping:
+                    var leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -1, 0]);
+sorter.sort();
+console.log(sorter.collection);
