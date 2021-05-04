8cb31267e9cff2a49b52cf3628a50b25fde29de9
Martyna Markiel <mail@gft.com>
2021-05-04 16:59:53 +0200

    add google maps support

---
+++ maps/src/User.ts
@@ -0,0 +1,18 @@
+import faker from 'faker';
+// create the properties
+export class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  // initialise:
+  constructor() {
+    this.name = faker.name.firstName();
+    // need to initialise te location object and its properties
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+}
