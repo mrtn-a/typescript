2405f70a60a39ef551189f1556bbeb2aa4ed16ad
mrtn-a <martyna.markiel@gmail.com>
2021-05-05 15:39:40 +0200

    extract csv reading

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,21 @@
-console.log('hi there');
+import { CvsFileReader } from './CsvFileReader';
+
+const reader = new CvsFileReader('football.csv');
+reader.read();
+
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+let manUnitedWins = 0;
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
