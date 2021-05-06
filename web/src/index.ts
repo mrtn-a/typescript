e33815ec6b7a58aaf9f0bd7360e781c3f0c18d94
mrtn-a <martyna.markiel@gmail.com>
2021-05-06 18:17:00 +0200

    add fetch functionality

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,5 +1,6 @@
-import { User } from './models/User';
+import axios from 'axios';
 
-const user = new User({ name: 'myname', age: 20 });
-
-console.log(user.get('name'));
+axios.post('http://localhost:3000/users', {
+  name: 'myname',
+  age: 20,
+});
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,4 +1,7 @@
+import axios, { AxiosResponse } from 'axios';
+
 interface userProps {
+  id?: number;
   name?: string;
   age?: number;
 }
@@ -24,5 +27,25 @@ export class User {
     const handlers = this.events[eventName] || [];
     handlers.push(callback);
     this.events[eventName] = handlers;
+  }
+
+  trigger = (eventName: string): void => {
+    const handlers = this.events[eventName];
+    // if handler is undefined or there's no handlers
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+    // if there are handlers
+    handlers.forEach((callback) => {
+      callback();
+    });
+  };
+
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get('id')}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data);
+      });
   }
 }
