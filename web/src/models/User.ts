bf33bdd901288dbe6f31ca66a52a8d155e3585bc
mrtn-a <martyna.markiel@gmail.com>
2021-05-06 17:46:20 +0200

    trigger event callbacks

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -3,7 +3,13 @@ interface userProps {
   age?: number;
 }
 
+// type allias
+type Callback = () => void;
+
 export class User {
+  // object event of strings
+  events: { [key: string]: Callback[] } = {};
+
   constructor(private data: userProps) {}
 
   get(propName: string): number | string {
@@ -12,5 +18,11 @@ export class User {
 
   set(update: userProps): void {
     Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
   }
 }
