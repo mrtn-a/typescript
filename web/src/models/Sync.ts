26c8c9f40cbdebdfc878d6681c5b6834a39efbe4
mrtn-a <martyna.markiel@gmail.com>
2021-05-07 12:22:28 +0200

    reintegrate attributes

--- web/src/models/Sync.ts
+++ web/src/models/Sync.ts
@@ -1,9 +1,10 @@
 import axios, { AxiosPromise } from 'axios';
 
 interface HasId {
-  id: number;
+  id?: number;
 }
-export class Sync<T> {
+
+export class Sync<T extends HasId> {
   constructor(public rootUrl: string) {}
 
   fetch(id: number): AxiosPromise {
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,43 +1,22 @@
-import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
+import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 
-interface userProps {
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
 
+const rootUrl = 'http://localhost:3000/users';
 // only accept properties into constructor
 // hard code dependencies as class properties
 export class User {
   public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
 
-  constructor(private data: userProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(update: userProps): void {
-    Object.assign(this.data, update);
-  }
-
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
-  save(): void {
-    // extract id
-    const id = this.get('id');
-    if (id) {
-      // put
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      // no user yet; post
-      axios.post('http://localhost:3000/users', this.data);
-    }
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
   }
 }
