2ee3a42723084058a670f371280bf577d6800944
mrtn-a <martyna.markiel@gmail.com>
2021-05-11 16:22:52 +0200

    redux setup

--- react-redux-ts-app/src/index.tsx
+++ react-redux-ts-app/src/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
-class App extends React.Component {
-  render() {
-    return <div>Hi there!</div>
-  }
-}
-ReactDOM.render(<App />, document.querySelector('#root'));
+import { App } from "./components/App"
+import { reducers } from "./reducers"
+
+
+const store = createStore(reducers, applyMiddleware(thunk))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+   document.querySelector('#root')
+);
